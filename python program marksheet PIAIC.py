75482f767f03890b839355e96dbccc4c9c9efc0b
AxhhadAxeem <51041890+AxhhadAxeem@users.noreply.github.com>
2019-05-26 11:19:57 +0500

    marksheet program
    assignment given by sir anees

---
+++ python program marksheet PIAIC.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+#python program for making a MARKSHEET
+print ("Name:                      Ashhad")
+print ("roll number:                AXU987")
+print (          )
+print (          )
+print (          )
+print (          )
+
+maths_no = int(input("maths_no                 "))
+
+physics_no = int(input("physics_no               "))
+
+chem_no = int(input("chem_no                  "))
+
+total_marks = (maths_no + physics_no + chem_no)/3
+print ("                     ","_____")
+print (      )
+print(       )
+print ("                       ",total_marks, "%")
+
+if total_marks >= 90:
+    print ("Excellent")
+elif (total_marks) < 90 > 70:
+    print ("satisfactory")
+elif (total_marks) < 70:
+    print ("poor")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
